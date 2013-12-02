ee7d6c4eba163b5de52c48bce70a87f59850a592
inCodeSpace <box4goodmail@gmail.com>
2013-12-02 16:26:08 +0300

    all4news1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>all4news</title>
+</head>
+<body>
+	<?php echo "Hi! Хай";?>
+	<br> all4news - новости
+</body>
+</html>
+
