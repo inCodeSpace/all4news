f78756989ca0f32e02b50745db8505304c851905
inCodeSpace <box4goodmail@gmail.com>
2015-03-24 12:27:14 +0300

    add20

--- _protected/models/News.php
+++ _protected/models/News.php
@@ -56,29 +56,26 @@ class News extends \yii\db\ActiveRecord
     {
         if ($this->title === '') { // Если название не устанавливалось (при заполн. формы)
             // взять title сходив по данному url
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $this->url);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            $grab = curl_exec($curl);
-            curl_close($curl);
-            if (!$grab) { // если не может обработать ссылку
+            $grab = @file_get_contents($this->url);
+            if ($grab) { // если успешная обработка
+                // Подстановка рег. выражения (все что внутри тегов 2-х типов и s - если таблуция внутри title)
+                $pattern = '#(<title>(.*)</title>)|(<TITLE>(.*)</TITLE>)#s';
+                preg_match($pattern, $grab, $matches);
+                $new_title = trim($matches[2] . $matches[4]); // подставится 1-о из значений (результат для <title> или <TITLE>)
+                if ($new_title === '') { // если title не удалось извлечь
+                    $new_title = 'сайт'; // присвоить произвольное значение
+                }
+                // Обрезка вычисленного title (если превышен диапазон поля)
+                if (iconv_strlen($new_title, 'UTF-8') > 120) { // если больше поля в таблице БД
+                    //удалить пробелы в строке, обрезать и добавить многоточие в конец строки
+                    $new_title = iconv_substr(trim($new_title), 0, 118, 'UTF-8') . "..";
+                }
+                $this->title = $new_title;
+            } else {
                 // битая ссылка, ошибка соединения либо не может определить title
                 $this->addError('title', '(!) Не удается сформировать заголовок для данной ссылки');
                 return false;
             }
-            // Подстановка рег. выражения (все что внутри тегов 2-х типов и s - если таблуция внутри title)
-            $pattern = '#(<title>(.*)</title>)|(<TITLE>(.*)</TITLE>)#s';
-            preg_match($pattern, $grab, $matches);
-            $new_title = trim($matches[2] . $matches[4]); // подставится 1-о из значений (результат для <title> или <TITLE>)
-            if ($new_title === '') { // если title не удалось извлечь
-                $new_title = 'сайт'; // присвоить произвольное значение
-            }
-            // Обрезка вычисленного title (если превышен диапазон поля)
-            if (iconv_strlen($new_title, 'UTF-8') > 120) { // если больше поля в таблице БД
-                //удалить пробелы в строке, обрезать и добавить многоточие в конец строки
-                $new_title = iconv_substr(trim($new_title), 0, 118, 'UTF-8') . "..";
-            }
-            $this->title = $new_title;
         }
         return true;
     }
